320f70be13fb9943602995c51d89faf3a424c616
Igor Ryabenko <100082550+iryabenkoff@users.noreply.github.com>
2022-08-01 19:47:21 +0300

    day1

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,15 +1,17 @@
 import Logo from "./Logo";
 import HeaderAuth from "./HeaderAuth/HeaderAuth";
 import UserMenu from "./UserMenu";
-import Button from "../../shared/components/Button";
+import s from './header.module.scss';
+
 const Header = () => {
   return (
-    <header>
+    <header className={s.header}>
       <div className="container">
+        <nav className={s.nav}>
         <Logo />
-        <Button text="bdsads" />
         <HeaderAuth />
-        <UserMenu />
+        {/* <UserMenu /> */}
+        </nav>
       </div>
     </header>
   );
